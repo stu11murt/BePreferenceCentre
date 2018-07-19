6da573f7cc86e564a505404504b06a4c7482fc33
Stuart Murton <stuart@deepbluedevelopment.co.uk>
2018-07-19 12:46:54 +0100

    to be office

--- BePreferenceCentre/App_Start/BundleConfig.cs
+++ BePreferenceCentre/App_Start/BundleConfig.cs
@@ -10,6 +10,7 @@ namespace BePreferenceCentre
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
+                         "~/Scripts/popper.js",
                         "~/Scripts/summernote/summernote-bs4.js",
                         "~/Scripts/DataTables/jquery.dataTables.min.js",
                         "~/Scripts/DataTables/dataTables.bootstrap.min.js",
@@ -26,8 +27,7 @@ namespace BePreferenceCentre
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/popper.js",
-                      "~/Scripts/bootstrap.js",
+                     "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
